12ede2d17327dabf4de8e8041df5b360177606de
Ninjaa <ahlulffirdaus@gmail.com>
2023-11-06 22:28:00 +0700

    Update pdf_view.php

--- app/Views/generate_laporan/pdf_view.php
+++ app/Views/generate_laporan/pdf_view.php
@@ -1,11 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Laporan</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
+
 <body>
-    Halo gesss
+    <div class="container">
+        <h1>Laporan Pengaduan</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Tanggal</th>
+                    <th>Produk</th>
+                    <th>Jumlah</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>2023-11-01</td>
+                    <td>Produk A</td>
+                    <td>10</td>
+                    <td>500.00</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>2023-11-02</td>
+                    <td>Produk B</td>
+                    <td>8</td>
+                    <td>400.00</td>
+                </tr>
+                <!-- Tambahkan baris data sesuai kebutuhan -->
+            </tbody>
+        </table>
+    </div>
 </body>
+
 </html>
